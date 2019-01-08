dec18e8bb10062fc5c46591b01b7253e3795ffe6
scriptfuture <progratronic@gmail.com>
2019-01-09 01:13:40 +0300

    fix br

--- src/App.js
+++ src/App.js
@@ -31,9 +31,6 @@ const App = () => (
                      
                     </div>
                 </div>
-
-                <br />
-
             </main>
             <Footer />
         </div>
